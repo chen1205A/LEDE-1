8b07a78f0464200a42936dd0ec91674da4e0e9a7
tcsr200722 <350524166@qq.com>
2020-03-04 21:43:52 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,8 +7,11 @@
 #=================================================
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
-#add app
+#add luci-app-koolproxyR
 git clone https://github.com/tcsr200722/luci-app-koolproxyR.git package/luci-app-koolproxyR
-#uci-app-diskman
+#add luci-app-diskman
 mkdir -p package/luci-app-diskman && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
 mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
+#upgrade feeds
+./scripts/feeds update -a
+./scripts/feeds install -a
