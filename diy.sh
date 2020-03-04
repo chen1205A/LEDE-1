da17a875dd08ca4c43a2a481c825ca69326a57d3
tcsr200722 <350524166@qq.com>
2020-03-04 18:30:49 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,6 @@
 sed -i 's/192.168.1.1/1/g' package/base-files/files/bin/config_generate
 #add app
 git clone https://github.com/tcsr200722/luci-app-koolproxyR.git package/luci-app-koolproxyR
+#uci-app-diskman
+mkdir -p package/luci-app-diskman && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
+mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
