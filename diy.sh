5b0727eecf8f0570a90b6bc1cf49c871ed4e382c
tcsr200722 <350524166@qq.com>
2020-03-10 23:12:07 +0800

    更换luci-app-koolproxyR版本

--- diy.sh
+++ diy.sh
@@ -8,7 +8,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/10.10.10.1/g' package/base-files/files/bin/config_generate
 #add luci-app-koolproxyR
-git clone https://github.com/tcsr200722/luci-app-koolproxyR.git package/luci-app-koolproxyR
+git clone https://github.com/Leo-Jo/luci-app-koolproxyR.git package/luci-app-koolproxyR
 #add luci-app-diskman
 mkdir -p package/luci-app-diskman && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Makefile -O package/luci-app-diskman/Makefile
 mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O package/parted/Makefile
@@ -16,8 +16,6 @@ mkdir -p package/parted && wget https://raw.githubusercontent.com/lisaac/luci-ap
 git clone https://github.com/project-openwrt/FileBrowser.git package/luci-app-filebrowser
 #add OpenAppFilter
 git clone https://github.com/project-openwrt/OpenAppFilter.git package/OpenAppFilter
-#add luci-app-syncthing tmp
-#git clone https://github.com/tcsr200722/luci-app-syncthing.git package/luci-app-syncthing
 #upgrade feeds
 ./scripts/feeds update -a
 ./scripts/feeds install -a
